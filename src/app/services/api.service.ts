859e7292c8378a80efd4ce85554e7f83960d5686
FabioKenzo <fabiok1999@hotmail.com>
2026-08-02 19:14:59 -0300

    "fix: deixando a url para a render"

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -24,8 +24,8 @@ export interface ServicoDTO {
 })
 export class ApiService {
 
-  //private baseUrl = 'https://catalogoservicos.onrender.com';
-  baseUrl = 'https://20.172.158.126:8080';
+  private baseUrl = 'https://catalogoservicos.onrender.com';
+  //private baseUrl = 'http://20.172.158.126:8080/api'; //link que foi utilizado para azure(teste ja excluido da plataforma)
 
   constructor(private http: HttpClient) { }
 
